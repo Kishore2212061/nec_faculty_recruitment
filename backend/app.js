8123a333af36082b34d23742134448546f63705b
Shyam Shunmugam G <mayhsshunmugam@gmail.com>
2025-03-25 16:29:31 +0530

    added publication bot frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import educationRoutes from './routes/educationRoutes.js';
 import phdRoutes from './routes/phdRoutes.js';
 import courseRoutes from './routes/courseRoutes.js';
 import userinfoRoutes from './routes/userInfoRoutes.js';
+import publicationRoutes from './routes/publicationRoutes.js';
 import path from 'path';
 
 const app = express();
@@ -20,6 +21,7 @@ app.use("/api/education", educationRoutes);
 app.use("/api/phd", phdRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/user-info', userinfoRoutes);
+app.use('/api/publications',publicationRoutes);
 app.use('/api/', experienceRoutes);
 
 app.listen(5000, () => console.log('Server running on port 5000'));
