d49aa03bab3e46f95cf1ec1aa386c4d9760d66be
kishore2212061 <mersalkishore79@gmail.com>
2025-03-30 12:38:37 +0530

    vite--host included

--- my-vue-app/vite.config.ts
+++ my-vue-app/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),    tailwindcss(),],
+  server: {
+    host: true,  // This ensures exposure
+    port: 5173,  // Ensure it's set
+  },
 })
